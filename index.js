0567e934f884a7a15aeed960f24c717b6708386e
Didka Birova <didka.birova@oryxgaming.com>
2017-03-15 21:22:44 +0100

    added name instead of human

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-for (var i=0; i < 5; i++) {
-  console.log("Hi human " + i);
+for (var i=0; i < 4; i++) {
+  console.log("Hi Didka " + i);
 }
 
 console.log("Total hi's " + i);
 
 for (var j=i; j>0; j--) {
- console.log("Bye human: " + j);
+ console.log("Bye Didka: " + j);
 }
 
 console.log("Total bye's " + j);
