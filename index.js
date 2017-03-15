2226456ce31728fbf6177928c346a629174644a4
Didka Birova <didka.birova@oryxgaming.com>
2017-03-15 22:15:27 +0100

    changed to my name

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ for (var i=0; i < 10; i++) {
 console.log("Total hi's " + i);
 
 for (var j=i; j>0; j--) {
- console.log("Bye human: " + j);
+ console.log("Bye Didka: " + j);
 }
 
 console.log("Total bye's " + j);
